4d17912f0434bcefc9230e1202e8324b5aa49c67
Jason Arnold <thejasonfile@users.noreply.github.com>
2016-10-28 12:26:46 -0400

    remove incorrect resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  resources :delete_listing_id_from_tools
-  resources :add_lender_id_from_tools
-  resources :remove_listing_id_from_tools
   root 'welcome#index'
   get '/logout', to: 'sessions#destroy'
   resources :users
