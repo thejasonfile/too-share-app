d5b7517f06fa4e52cd148a3bd45fc42382150e54
Jason Arnold <jason.arnold.00@gmail.com>
2016-10-29 11:21:41 -0400

    add listing to user show, wrote more rspec tests

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, :type => :controller do
+
   describe "GET #new" do
     it "responds successfully with an HTTP 200 status code" do
       get :new
@@ -14,25 +15,36 @@ RSpec.describe UsersController, :type => :controller do
     end # it
   end #describe
 
-  #let(:user) {FactoryGirl.create :user}
-
   describe "POST #create" do
     it "creates a user" do
       expect { post :create, user: FactoryGirl.attributes_for(:user) }.to change(User, :count).by(1)
     end #it
 
-    it "creates the session id if successfully saved" do
+    # it "the session id is set to match the user id" do
+    #   expect {post :create, user: FactoryGirl.attributes_for(:user) }.to 
+    # end #it
 
-    end #it
+    # it "redirects to user_path if successfully saved" do
 
-    it "redirects to user_path if successfully saved" do
-
-    end #it
+    # end #it
 
     it "renders the new template if not saved" do
     get :new
     expect(response). to render_template("new")
     end #it
+  end #describe
+
+  describe "GET #show" do
+    it "responds successfully with an HTTP 200 status code" do
+      get :show
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end # it
+
+    it "renders the new template" do
+      get :show
+      expect(response).to render_template("show")
+    end # it
   end #describe
 
 end #rspec
