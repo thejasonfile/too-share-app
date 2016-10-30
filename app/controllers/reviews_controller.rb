9b16318bdb5895f3ce0ecd4ca34396454ecc2fd7
Niti Nrw <niti.patel@flatironschool.com>
2016-10-29 21:21:27 -0400

    Reviews controller is done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,7 @@
 class ReviewsController < ApplicationController
   def index
     @reviews = Review.all
+    #think we don't need this? So there is no template
   end
 
   def new
@@ -12,7 +13,8 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.create!(rating: params[:review][:rating], content: params[:review][:content], tool_id: params[:review][:tool_id])
     if @review.save
-      redirect_to review_path(@review)
+      @tool = Tool.find(@review.tool_id)
+      redirect_to tool_path(@tool)
     else
       render :new
     end
@@ -23,10 +25,26 @@ class ReviewsController < ApplicationController
   end
 
   def edit
+    @review = Review.find(params[:id])
   end
+
   def update
+    @review = Review.find(params[:id])
+    @tool = Tool.find(@review.tool_id)
+    @review.update(rating: params[:review][:rating], content: params[:review][:content])
+    if @review.valid?
+      @review.save
+      redirect_to tool_path(@tool)
+    else
+      render :edit
+    end
   end
+
   def destroy
+    @review = Review.find(params[:id])
+    @tool = Tool.find(@review.tool_id)
+    @review.delete
+    redirect_to tool_path(@tool)
   end
 
   private
