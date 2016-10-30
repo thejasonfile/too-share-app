8d3e51ef118ed05d6568301c6af060990af7a7ee
Niti Nrw <niti.patel@flatironschool.com>
2016-10-30 13:48:03 -0400

    Drop checkouts stuff, has review rspec tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161029200632) do
+ActiveRecord::Schema.define(version: 20161030174702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "checkouts", force: :cascade do |t|
-    t.integer  "total_cost"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "listings", force: :cascade do |t|
     t.integer  "cost"
--- spec/factories/create_checkouts.rb
+++
@@ -1,5 +0,0 @@
-FactoryGirl.define do
-  factory :create_checkout do
-    
-  end
-end
