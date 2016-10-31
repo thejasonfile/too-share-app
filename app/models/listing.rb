a298c90a6e0770d4a7b4c16fbdbe301f219b4980
Jason Arnold <jason.arnold.00@gmail.com>
2016-10-31 16:47:56 -0400

    add map to search

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -14,8 +14,29 @@ class Listing < ApplicationRecord
     self.find_by_sql(sql)
   end
 
+  def find_lender
+    self.tool.lender
+  end
+
   def tool_name
     Tool.find(self.tool_id).name
   end
 
+  def self.by_zip_code(zip_code)
+    sql = <<-SQL
+      SELECT users.zip_code, tools.name FROM listings
+      JOIN tools ON listings.tool_id = tools.id
+      JOIN users ON tools.lender_id = users.id
+      WHERE users.zip_code = #{zip_code}
+    SQL
+    self.find_by_sql(sql)
+  end
+
+  def map_string
+    @map_string = "https://maps.googleapis.com/maps/api/staticmap?center="
+    @map_string += "#{self.find_lender.location}&zoom=13&size=400x400&"
+    @map_string += "markers=color:blue%7Clabel:S"
+
+    @map_string
+  end
 end
