ccac9e873364e477e41bf89a8deb1fc17a8a7d5b
benjaminhenriquez <henriquez.ben@gmail.com>
2017-03-06 21:56:40 -0500

    Update welcome_controller.rb

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,4 +2,8 @@ class WelcomeController < ApplicationController
   skip_before_action :require_login
   def index
   end
+    def iframe_action
+    response.headers.delete "X-Frame-Options"
+    render_something
+  end
 end
